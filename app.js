75c776e68a8a446a78dbcfc859e41d8110151e6a
Mike Murphy <foriamyourgod@hotmail.com>
2017-11-20 11:48:51 -0800

    added colors npm

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
+const colors = require('colors');
 //my local requirements
 const jobAPI = require('./jobs.js'); //connects to your job.js API file
 
@@ -20,5 +21,5 @@ if (command === 'add'){
 } else if (command === 'delete') {
   jobAPI.deleteJob(argv.title, argv.jobN);
 } else {
-  console.log('Task Unknown');
+  console.log('Task Unknown'.bold.red);
 }
--- jobs.js
+++ jobs.js
@@ -1,22 +1,22 @@
-console.log('Starting up the microbase...');
+console.log('Starting up the microbase...'.underline.bold.blue);
 
 
 var addJob = (title, jobN) => {
-  console.log('Adding Job:', title);
-  console.log('Adding Job Number:', jobN);
+  console.log('Adding Job:'.bold, title);
+  console.log('Adding Job Number:'.bold, jobN);
 };
 
 var getAll = () => {
-  console.log('Getting all Jobs:');
+  console.log('Getting all Jobs:'.bold);
 };
 
 var readJob = (title, jobN) => {
-  console.log('Read Out:');
+  console.log('Read Out:'.bold);
 
 };
 
 var deleteJob = (title, jobN) => {
-  console.log('Deleting Job:',title, 'Associated Job Number:', jobN);
+  console.log('Deleting Job:'.bold,title, 'Associated Job Number:'.bold, jobN);
 };
 
 
